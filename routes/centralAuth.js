28b5f1bcf4e9a66db7b34bbd00929789c99f8409
l-gorman <lgorman@turing.ac.uk>
2022-10-25 13:46:29 +0100

    Adds logs to central authentication route

--- routes/centralAuth.js
+++ routes/centralAuth.js
@@ -6,6 +6,16 @@ const Log = require('../models/Log')
 
 async function getCentralAuthToken() {
     try {
+        log({
+            file: './routes/centralAuth.js',
+            line: '9',
+            info: {
+                message:'Getting central authentication token',
+                
+            },
+            type: 'message'
+        }, Log)
+        
         const central_token = await axios({
             url: process.env.CENTRAL_URL + "/v1/sessions",
             method: "post",
@@ -15,11 +25,42 @@ async function getCentralAuthToken() {
             }
         })
         // console.log(central_token)
-        if (central_token.data === undefined) throw "Could not obtain central auth token"
-        if (central_token.data.token === undefined) throw "Could not obtain central auth token"
+        if (central_token.data === undefined) {
+            log({
+                file: './routes/centralAuth.js',
+                line: '30',
+                info: {
+                    message:'Unable to get central token',
+                    
+                },
+                type: 'message'
+            }, Log)
+            throw "Could not obtain central auth token"
+        }
+        if (central_token.data.token === undefined) {
+            log({
+                file: './routes/centralAuth.js',
+                line: '42',
+                info: {
+                    message:'Unable to get central token',
+                    
+                },
+                type: 'message'
+            }, Log)
+            throw "Could not obtain central auth token"
+        }
 
         return central_token.data.token
     } catch (err) {
+        log({
+            file: './routes/centralAuth.js',
+            line: '55',
+            info: {
+                message:'Unable to get central token',
+                
+            },
+            type: 'message'
+        }, Log)
         return "Could not obtain central token, err:" + err
     }
 }
